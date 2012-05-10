d4bfe62bddd23c5f6decc63b23f1f44e23abacbc
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-09 18:18:42 -0700

    CORE-3168: defined some more entities

--- src/kameleon/entities.clj
+++ src/kameleon/entities.clj
@@ -4,7 +4,9 @@
 
 (declare users workspace template_group transformation_activity
          integration_data deployed_components deployed_component_data_files
-         transformation_steps)
+         transformation_steps transformations output_mapping input_mapping
+         template inputs outputs info_type data_formats multiplicity
+         property_group property property_type)
 
 ;; Users who have logged into the DE.
 (defentity users
@@ -61,6 +63,105 @@
   (entity-fields :filename :input_file)
   (belongs-to deployed_components {:fk :deployed_component_id}))
 
-;; A list of steps within an app.
+;; Steps within an app.
 (defentity transformation_steps
-  (entity-fields :name :guid :description))
+  (entity-fields :name :guid :description)
+  (belongs-to transformations {:fk :transformation_id})
+  (has-many output_mapping {:fk :source})
+  (has-many input_mapping {:fk :target}))
+
+;; Transformations applied to steps within an app.
+(defentity transformations
+  (entity-fields :name :description :template_id))
+
+;; A table that maps outputs from one step to inputs to another set.  Two
+;; entities are associated with a single table here for convenience.  when I
+;; have more time, I'd like to try to improve the relation handling in Korma
+;; so that multiple relationships with the same table work correctly.
+(defentity output_mapping
+  (pk :hid)
+  (table :input_output_mapping :output_mapping))
+(defentity input_mapping
+  (pk :hid)
+  (table :input_output_mapping :input_mapping))
+
+;; Data object mappings can't be implemeted as entities until Korma supports
+;; composite primary keys.  In the meantime, we'll have to deal with this table
+;; in code.
+
+;; A template defines an interface to a tool that can be called.
+(defentity template
+  (pk :hid)
+  (entity-fields :id :name :description :label :type :component_id)
+  (many-to-many inputs :template_input
+                {:lfk :template_id
+                 :rfk :input_id})
+  (many-to-many outputs :template_output
+                {:lfk :template_id
+                 :rfk :output_id})
+  (many-to-many property_group :template_property_group
+                {:lfk :template_id
+                 :rfk :property_group_id}))
+
+;; Input and output definitions.  Once again, two entities are associated with
+;; the same table to allow us to define multiple relationships between the same
+;; two tables.
+(defentity inputs
+  (pk :hid)
+  (table :dataobjects :inputs)
+  (entity-fields :id :name :label :orderd :switch :description :required
+                 :retain :is_implicit)
+  (belongs-to info_type {:fk :info_type})
+  (belongs-to data_formats {:fk :data_format})
+  (belongs-to multiplicity {:fk :multiplicity}))
+(defentity outputs
+  (pk :hid)
+  (table :dataobjects :outputs)
+  (entity-fields :id :name :label :orderd :switch :description :required
+                 :retain :is_implicit)
+  (belongs-to info_type {:fk :info_type})
+  (belongs-to data_formats {:fk :data_format})
+  (belongs-to multiplicity {:fk :multiplicity}))
+(defentity data_object
+  (pk :hid)
+  (table :dataobjects :data_object)
+  (entity-fields :id :name :label :orderd :switch :description :required
+                 :retain :is_implicit)
+  (belongs-to info_type {:fk :info_type})
+  (belongs-to data_formats {:fk :data_format})
+  (belongs-to multiplicity {:fk :multiplicity}))
+
+;; The type of information stored in a data object.
+(defentity info_type
+  (pk :hid)
+  (entity-fields :id :name :label :description :deprecated :display_order))
+
+;; The format of the data in a data object.
+(defentity data_formats
+  (entity-fields :guid :name :label :display_order))
+
+;; An input or output multiplicity definition.
+(defentity multiplicity
+  (pk :hid)
+  (entity-fields :id :name :label :description :type_name))
+
+;; A group of properties.
+(defentity property_group
+  (pk :hid)
+  (entity-fields :id :name :description :label :group_type :is_visible)
+  (many-to-many property :property_group_property
+                {:lfk :property_group_id
+                 :rfk :property_id}))
+
+;; A single property.
+(defentity property
+  (pk :hid)
+  (entity-fields :id :name :description :label :defalut_value :is_visible
+                 :ordering :omit_if_blank)
+  (belongs-to data_object {:fk :dataobject_id})
+  (belongs-to property_type {:fk :property_type}))
+
+;; The type of a single property.
+(defentity property_type
+  (pk :hid)
+  (entity-fields :id :name :description :label :deprecated :display_order))
