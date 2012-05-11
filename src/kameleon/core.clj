a2865363761f1cda374cbddfa5982bc3dd397046
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-10 17:51:14 -0700

    CORE-3168: added docstrings to core.clj

--- src/kameleon/core.clj
+++ src/kameleon/core.clj
@@ -11,6 +11,7 @@
             [korma.core :as kc]))
 
 (defn many-to-many-keys
+  "Determines and returns the keys needed for a many-to-many relationship."
   [parent child {:keys [join-table lfk rfk]}]
   {:lpk (kc/raw (eng/prefix parent (:pk parent)))
    :lfk (kc/raw (eng/prefix {:table (name join-table)} lfk))
@@ -19,11 +20,15 @@
    :join-table join-table})
 
 (defn db-keys-and-foreign-ent
+  "Determines and returns the database keys and foreign entity for a
+   relationship."
   [type ent sub-ent opts]
   (condp = type
     :many-to-many [(many-to-many-keys ent sub-ent opts) sub-ent]))
 
 (defn kameleon-create-rel
+  "Creates a relationship.  This is just like korma.core/create-rel
+   except that it supports many-to-many relationships."
   [ent sub-ent type opts]
   (let [[db-keys foreign-ent] (db-keys-and-foreign-ent type ent sub-ent opts)
         opts (when (:fk opts)
@@ -35,6 +40,8 @@
            opts)))
 
 (defn kameleon-rel
+  "Creates a relationship and associates it with an entity.  This is just like
+   korma.core/rel except that it supports many-to-many relationships."
   [ent sub-ent type opts]
   (let [var-name (-> sub-ent meta :name)
         cur-ns *ns*]
@@ -48,6 +55,7 @@
                  (kameleon-create-rel ent sub-ent type opts))))))
 
 (defn default-fk-name
+  "Determines the default name to use for a foreign key."
   [ent]
   (if (string? ent)
     (keyword (str ent "_id"))
@@ -65,6 +73,9 @@
                    :rfk (:rfk ~opts (default-fk-name ~sub-ent)))))
 
 (defn add-joins
+  "Adds join clauses to a query for a relationship.  If the relationship uses a
+   join table then two clauses will be added.  Otherwise, only one clause will
+   be added."
   [query ent rel]
   (if-let [join-table (:join-table rel)]
     (-> query
@@ -73,6 +84,8 @@
     (kc/join* query :left ent (sfns/pred-= (:pk rel) (:fk rel)))))
 
 (defmacro kameleon-join
+  "Adds join clauses to a query for a relationship.  This is identical to
+   korma.core/join except that it support many-to-many relationships."
   ([query ent]
      `(let [q# ~query
             e# ~ent
@@ -83,23 +96,31 @@
   ([query type table clause]
      `(kc/join* ~query ~type ~table (eng/pred-map ~(eng/parse-where clause)))))
 
-(defn- force-prefix [ent fields]
+(defn- force-prefix
+  "Copied from korma.core because it's private."
+  [ent fields]
   (for [field fields]
     (if (vector? field)
       [(utils/generated (eng/prefix ent (first field))) (second field)]
       (eng/prefix ent field))))
 
-(defn- add-aliases [query as]
+(defn- add-aliases
+  "Copied from korma.core because it's private."
+  [query as]
   (update-in query [:aliases] set/union as))
 
-(defn- merge-query [query neue]
+(defn- merge-query
+  "Copied from korma.core because it's private."
+  [query neue]
   (let [merged (reduce #(kc/merge-part % neue %2)
                        query
                        [:fields :group :order :where :params :joins :post-queries])]
     (-> merged
         (add-aliases (:aliases neue)))))
 
-(defn- sub-query [query sub-ent func]
+(defn- sub-query
+  "Copied from korma.core because it's private."
+  [query sub-ent func]
   (let [neue (kc/select* sub-ent)
         neue (eng/bind-query neue (func neue))
         neue (-> neue
@@ -109,6 +130,8 @@
     (merge-query query neue)))
 
 (defn- with-many-to-many
+  "Defines the post-query to be used to obtain entities in a many-to-many
+   relationship with entities in the current query."
   [rel query ent func opts]
   (let [{:keys [lfk rfk rpk join-table]} rel
         pk (get-in query [:ent :pk])
@@ -123,6 +146,10 @@
                             (kc/where {lfk (get % pk)})))))))
 
 (defn- with-later-fn
+  "Returns a function to be used to obtain entities in a relationship with
+   entities in the current query lazily.  This also allows the entities to be
+   retrieved as separate objects.  This function is used for has-many
+   relationships."
   [table ent func known unknown]
   (partial
    map #(assoc % table
@@ -130,25 +157,41 @@
                           (kc/where {unknown (get % known)})))))
 
 (defn- with-one-later-fn
+  "Returns a function to be used to obtain entities in a relationship with
+   entities in the current query lazily.  This also allows the entities to be
+   retrieved as separate objects.  This function is used for has-one and
+   belongs-to relationships."
   [table ent func known unknown]
   (partial
    map #(assoc % table
                (first (kc/select ent (func)
                                  (kc/where {unknown (get % known)}))))))
 
-(defn- with-later [rel query ent func opts]
+(defn- with-later
+  "Defines the post-query to be used to obtain entities in a has-many
+   relationship with entities in the current query lazily.  This also allows
+   the entities to be retrieved as separate objects."
+  [rel query ent func opts]
   (let [fk (:fk rel)
         pk (get-in query [:ent :pk])
         table (keyword (eng/table-alias ent))]
     (kc/post-query query (with-later-fn table ent func pk fk))))
 
-(defn- with-one-later [rel query ent func opts]
+(defn- with-one-later
+  "Defines the post-query to be used to obtain entities in has-one or
+   belongs-to relationships with entities in the current query lazily.  This
+   also allows the entities to be retrieved as separate objects."
+  [rel query ent func opts]
   (let [fk (:fk rel)
         pk (get-in query [:ent :pk])
         table (keyword (eng/table-alias ent))]
     (kc/post-query query (with-one-later-fn table ent func pk fk))))
 
-(defn- with-now [rel query ent func opts]
+(defn- with-now
+  "Defines the join to be used to obtain entries in has-one or belongs-to
+   relationships with entities in the current query eagerly.  In this case,
+   columns in the joined tables are included in the primary entity object."
+  [rel query ent func opts]
   (let [table (if (:alias rel)
                 [(:table ent) (:alias ent)]
                 (:table ent))
@@ -156,6 +199,12 @@
     (sub-query query ent func)))
 
 (defn- with-has-one
+  "Defines the post-query or join to be used to obtain entities in a has-one
+   relationship with entities in the current query.  If the :later option is
+   enabled then the entities will be retrieved lazily and will be returned as
+   separate objects.  Otherwise, the entities will be retrieved eagerly and the
+   columns in the entities will be included in the objects associated with the
+   entities in the query."
   [rel query ent func opts]
   (if (:later opts)
     (with-one-later rel query ent func opts)
@@ -163,6 +212,9 @@
 
 ;; FIXME: this will only work with the default naming strategy.
 (defn- extract-field-keyword
+  "Extracts the field keyword from a generated field name.  This method is
+   broken in that it will only work with the default naming strategy.
+   Fortunately, we're using the default naming strategy."
   [field]
   (let [{:keys [delimiters]} (or eng/*bound-options* @conf/options)
         [begin end] delimiters
@@ -171,6 +223,9 @@
     (keyword (string/replace quoted-name regex ""))))
 
 (defn- with-belongs-to-later
+  "Defines the post-query to be used to obtain entities in a belongs-to
+   relationship with entities in the current query.  This also allowd the
+   entities to be retrieved as separate objects."
   [rel query ent func opts]
   (let [fk (extract-field-keyword (:fk rel))
         pk (:pk rel)
@@ -178,6 +233,12 @@
     (kc/post-query query (with-one-later-fn table ent func fk pk))))
 
 (defn- with-belongs-to
+  "Defines the post-query or join to be used to obtain entities in a belongs-to
+   relationship with entities in the current query.  If the :later option is
+   enabled then the entities will be retrieved lazily and will be returned as
+   separate objects.  Otherwise, the entities will be retrieved eagerly and the
+   columns in the entities will be included in the objects associated with the
+   entities in the query."
   [rel query ent func opts]
   (if (:later opts)
     (with-belongs-to-later rel query ent func opts)
@@ -190,6 +251,10 @@
    :belongs-to with-belongs-to})
 
 (defn kameleon-with*
+  "Allows related entities to be fetched along with the query results.  This
+   function is identical to korma.core/with* except that it also supports
+   many-to-many relationships and lazy loading of objects in has-one and
+   belongs-to relationships."
   [query sub-ent func opts]
   (let [rel (kc/get-rel (:ent query) sub-ent)
         handler-fn (get with-handlers (:rel-type rel))]
@@ -198,12 +263,18 @@
     (handler-fn rel query sub-ent func opts)))
 
 (defmacro kameleon-with
+  "Allows related entities to be fetched along with the query results.  This
+   macro is identical to korma.core/with except that it also supports
+   many-to-many relationships."
   [query ent & body]
   `(kameleon-with* ~query ~ent (fn [q#]
                                  (-> q#
                                      ~@body)) {}))
 
 (defmacro with-object
+  "Allows related entities to be fetched along with the query results.  This
+   macro is identical to korma.core/with except that it also supports lazy
+   loading of entities in has-one and belongs-to relationships."
   [query ent & body]
   `(kameleon-with* ~query ~ent (fn [q#]
                                  (-> q#
