677f8a9f8477f248bf33aebc75cc0fe9c55cc5ba
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-14 15:32:07 -0700

    CORE-3176: added lein-marginalia to the list of plugins

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [korma "0.3.0-beta7"]
                  [postgresql "9.0-801.jdbc4"]
-                 [slingshot "0.10.2"]])
+                 [slingshot "0.10.2"]]
+  :plugins [[lein-marginalia "0.7.0"]])
