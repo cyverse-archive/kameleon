a853f273af8657393afa7a32ab3511d0a3201fd1
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-09-14 16:10:34 -0700

    Bumped up the korma version to beta11, added the iplant maven repo.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject org.iplantc/kameleon "0.0.1-SNAPSHOT"
+(defproject org.iplantc/kameleon "0.0.2-SNAPSHOT"
   :description "Library for interacting with backend relational databases."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [korma "0.3.0-beta10"]
+                 [korma "0.3.0-beta11"]
                  [postgresql "9.0-801.jdbc4"]
                  [slingshot "0.10.2"]]
   :plugins [[lein-marginalia "0.7.0"]]
-  :manifest {"db-version" "1.4.0:20120822.01"})
+  :manifest {"db-version" "1.4.0:20120822.01"}
+  :repositories {"iplantCollaborative"
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
