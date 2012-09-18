6aba32ed73449744f660227d6b888b2d699798fb
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-09-18 14:23:01 -0700

    Extended the (query-events) function so that it can filter results by the event source uuid. Also extended the same function so that the event source uuid can be included in the results easily. Added more code documentation.

--- src/kameleon/esp_queries.clj
+++ src/kameleon/esp_queries.clj
@@ -8,10 +8,23 @@
   "Utility function that looks in lookup-map for lookup-key and merges that
    key/value pair into merged-map if it exists in lookup-map. Used to
    generate the Korma where clauses."
-  [merged-map lookup-map lookup-key]
-  (if (contains? lookup-map lookup-key)
-    (assoc merged-map lookup-key (get lookup-map lookup-key))
-    merged-map))
+  ([merged-map lookup-map lookup-key]
+     (merge-field merged-map lookup-map lookup-key lookup-key))
+  
+  ([merged-map lookup-map lookup-key merge-as-key]
+     (if (contains? lookup-map lookup-key)
+       (assoc merged-map merge-as-key (get lookup-map lookup-key))
+       merged-map)))
+
+(defn xform-result
+  "Utility function. It takes in a result map from a query, a key for a field in
+   the result map, and a function that should be applied to the value
+   associated with the key in the result map. This is used in other locations
+   to turn java.util.UUID instances into strings, for instance."
+  [res-map key new-val-func]
+  (if (contains? res-map key)
+    (assoc res-map key (new-val-func (get res-map key)))
+    res-map))
 
 (defn convert-map
   "Utility function that converts the value associated with cv-key in
@@ -59,6 +72,17 @@
                    :source_data data
                    :source_uuid (str->pg-uuid src-uuid)})))
 
+(defn xform-es-map
+  "Cleans up a result map for an event_source query by (dissoc)ing the :id
+   field and turning the :source_uuid, :date_modified, and :date_created fields
+   into strings."
+  [es-map]
+  (-> es-map
+      (dissoc :id)
+      (xform-result :source_uuid #(str %1))
+      (xform-result :date_modified #(str %1))
+      (xform-result :date_created #(str %1))))
+
 (defn query-event-sources
   "Allows you to query the event_sources table in the database. Here are some
    example calls:
@@ -74,12 +98,15 @@
    returned just like you can with Korma queries.
 
    String uuids are converted to PostgreSQL UUIDs. Do not call (str->pg-uuid)
-   on them before passing them in."
+   on them before passing them in.
+
+   Note: This will never return the :id field. If you need the :id field, use
+   Korma's normal (select)."
   [es-fields & {:as opt-fields}]
-  (-> (select* event-sources)
-      (query-fields es-fields)
-      (event-source-where (convert-uuids opt-fields))
-      (select)))
+  (map xform-es-map (-> (select* event-sources)
+                         (query-fields es-fields)
+                         (event-source-where (convert-uuids opt-fields))
+                         (select))))
 
 (defn event-source-id
   "Helper method that queries the database for the id associated with the
@@ -96,20 +123,58 @@
   [query opt-fields]
   (if (zero? (count (keys opt-fields)))
     query
-    (where query (-> {}
-                     (merge-field opt-fields :event_uuid)
-                     (merge-field opt-fields :event_sources_id)
-                     (merge-field opt-fields :event_data)))))
+    (where query
+           (-> {}
+               (merge-field opt-fields :event_uuid)
+               (merge-field opt-fields :source_uuid :event_sources.source_uuid)
+               (merge-field opt-fields :event_data)
+               (merge-field opt-fields :event_type)))))
 
 (defn insert-event
   "Inserts an event into the database. ev-uuid is a string containing the uuid
    to associate with the event. scr-uuid is a string containing the uuid of the
    event_source associate with this event. event_data is a string containing
    arbitrary event data."
-  [ev-uuid src-uuid data]
+  [ev-uuid src-uuid ev-type data]
   (insert events (values {:event_uuid (str->pg-uuid ev-uuid)
                           :event_sources_id (event-source-id src-uuid)
-                          :event_data data})))
+                          :event_data data
+                          :event_type ev-type})))
+
+(defn xform-event-map
+  "Takes in a result map for an event query and cleans it up. It does this by
+   (dissoc)ing any fields that are database specific (namely :event_sources_id
+   and :id) and by turning the :event_uuid, :source_uuid, :date_created, and
+   :date_modified fields into strings."
+  [ev-map]
+  (-> ev-map
+      (dissoc :event_sources_id :id)
+      (xform-result :event_uuid #(str %1))
+      (xform-result :source_uuid #(str %1))
+      (xform-result :date_modified #(str %1))
+      (xform-result :date_created #(str %1))))
+
+(defn xform-ef
+  "Takes in a field name that normally gets passed to the (fields) part of a
+   Korma query and translates it to :event_sources.source_uuid if it's
+   currently set to :source_uuid. Otherwise it gets left alone."
+  [ef]
+  (cond
+   (vector? ef)
+   (if (= (first ef) :source_uuid)
+     (apply (partial vector :event_sources.source_uuid) (rest ef))
+     ef)
+
+   (= ef :source_uuid)
+   :event_sources.source_uuid
+
+   :else
+   ef))
+
+(defn xform-event-fields
+  "Maps xform-ef to every value of event-fields."
+  [event-fields]
+  (map xform-ef event-fields))
 
 (defn query-events
   "Allows you to query the events table in the database. Here are some example
@@ -124,9 +189,15 @@
    and multiple key-value pairs can be included to form the where clause.
 
    String uuids are converted to PostgreSQL UUIDS. Do not call (str->pg-uuid)
-   on them before passing them in."
+   on them before passing them in.
+
+   Note: This function will never return the :id or :event_sources_id fields.
+   If you need either of those fields, use Korma's regular (select)."
   [event-fields & {:as opt-fields}]
-  (-> (select* events)
-      (query-fields event-fields)
-      (events-where (convert-uuids opt-fields))
-      (select)))
+  (into
+   []
+   (map xform-event-map (-> (select* events)
+                            (with event-sources (fields :source_uuid))
+                            (query-fields (xform-event-fields event-fields))
+                            (events-where (convert-uuids opt-fields))
+                            (select)))))
