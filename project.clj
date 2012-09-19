d8a264a6a6526e9c3d894f885dc8c2dafaaff535
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-09-19 12:35:23 -0700

    Added clojure.data.json as a dependency.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.iplantc/kameleon "0.0.2-SNAPSHOT"
   :description "Library for interacting with backend relational databases."
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.1"]
                  [korma "0.3.0-beta11"]
                  [postgresql "9.0-801.jdbc4"]
                  [slingshot "0.10.2"]]
