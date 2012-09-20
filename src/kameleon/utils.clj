5629f7fe6bbbcb9c206065a44cc9615d0df9138c
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-09-20 10:18:00 -0700

    Added a utility function for turning date strings into postgresql timestamps.

--- src/kameleon/utils.clj
+++ src/kameleon/utils.clj
@@ -4,3 +4,9 @@
 (defn str->pg-uuid
   [uuid-str]
   (raw (format "CAST('%s' AS uuid)" (name uuid-str))))
+
+(defn str->timestamp
+  ([ts-str]
+     (str->timestamp ts-str "YYYY-MM-DD HH24:MI:SS:US"))
+  ([ts-str fmt-str]
+     (raw (format "to_timestamp('%s', '%s')" (name ts-str) (name fmt-str)))))
