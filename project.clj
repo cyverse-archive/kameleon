a3291c8d38896b5de57b72d3405a88ab7dd9cb15
Dennis Roberts <dennis@iplantcollaborative.org>
2013-02-26 19:09:28 -0700

    CORE-3989: changed to the most recent version of Korma and bumped the compatible database version

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Library for interacting with backend relational databases."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [korma "0.3.0-RC2"]
+                 [korma "0.3.0-RC4"]
                  [postgresql "9.0-801.jdbc4"]
                  [slingshot "0.10.3"]]
   :plugins [[lein-marginalia "0.7.1"]]
-  :manifest {"db-version" "1.8.0:20130204.01"})
+  :manifest {"db-version" "1.8.0:20130225.01"})
