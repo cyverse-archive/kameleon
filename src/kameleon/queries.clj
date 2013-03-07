cff54ccb8e0badfc1b4fab61e67a4cf90012b5bd
Dennis Roberts <dennis@iplantcollaborative.org>
2013-03-07 15:23:15 -0700

    CORE-4032: added some queries related to tool requests

--- src/kameleon/queries.clj
+++ src/kameleon/queries.clj
@@ -189,3 +189,81 @@
   "Gets the user ID for the public user."
   []
   (:id (get-or-create-user "<public>")))
+
+(defn get-tool-request-details
+  "Obtains detailed information about a tool request."
+  [uuid]
+  (first
+   (select [:tool_requests :tr]
+           (fields :tr.uuid
+                   [:requestor.username :submitted_by]
+                   :tr.phone
+                   :tr.tool_name
+                   :tr.description
+                   :tr.source_url
+                   [:tr.doc_url :documentation_url]
+                   :tr.version
+                   :tr.attribution
+                   :tr.multithreaded
+                   [:architecture.name :architecture]
+                   :tr.test_data_path
+                   [:tr.instructions :cmd_line]
+                   :tr.additional_info
+                   :tr.additional_data_file)
+           (join [:users :requestor]
+                 {:tr.requestor_id :requestor.id})
+           (join [:tool_architectures :architecture]
+                 {:tr.tool_architecture_id :architecture.id})
+           (where {:tr.uuid uuid}))))
+
+(defn get-tool-request-history
+  "Obtains detailed information about the history of a tool request."
+  [uuid]
+  (select [:tool_request_statuses :trs]
+          (fields [:trsc.name :status]
+                  [:trs.date_assigned :status_date]
+                  [:updater.username :updated_by]
+                  :trs.comments)
+          (join [:tool_requests :tr]
+                {:trs.tool_request_id :tr.id})
+          (join [:users :updater]
+                {:trs.updater_id :updater.id})
+          (join [:tool_request_status_codes :trsc]
+                {:trs.tool_request_status_code_id :trsc.id})
+          (where {:tr.uuid uuid})
+          (order :trs.date_assigned :ASC)))
+
+(defn- list-tool-requests-subselect
+  "Creates a subselect query that can be used to list the tool requests for a user."
+  [user]
+  (subselect [:tool_requests :tr]
+             (fields [:tr.uuid :uuid]
+                     [:tr.tool_name :tool_name]
+                     [:tr.version :version]
+                     [:trsc.name :status]
+                     [:trs.date_assigned :status_date]
+                     [:updater.username :updated_by])
+             (join [:users :requestor] {:tr.requestor_id :requestor.id})
+             (join [:tool_request_statuses :trs] {:tr.id :trs.tool_request_id})
+             (join [:tool_request_status_codes :trsc]
+                   {:trs.tool_request_status_code_id :trsc.id})
+             (join [:users :updater] {:trs.updater_id :updater.id})
+             (where {:requestor.username user})
+             (order :trs.date_assigned :ASC)))
+
+(defn list-tool-requests
+  "Lists the tool requests that have been submitted by the user."
+  [user & {row-offset :offset
+           row-limit  :limit
+           sort-field :sort-field
+           sort-order :sort-order}]
+  (select [(list-tool-requests-subselect user) :req]
+          (fields :uuid :tool_name :version
+                  [(sqlfn :first :status_date) :date_submitted]
+                  [(sqlfn :last :status) :status]
+                  [(sqlfn :last :status_date) :date_updated]
+                  [(sqlfn :last :updated_by) :updated_by])
+          (group :uuid :tool_name :version)
+          (order (or sort-field :date_submitted) (or sort-order :ASC))
+          (limit row-limit)
+          (offset row-offset)))
