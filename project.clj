befe3011fc63046682f75b0c39e06e752d82b06f
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 16:22:39 -0700

    upped dependencies and increased version
    clojure -> 1.5.0
    tools.logging -> 0.2.6

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.iplantc/kameleon "0.1.0-SNAPSHOT"
+(defproject org.iplantc/kameleon "0.1.1-SNAPSHOT"
   :description "Library for interacting with backend relational databases."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [korma "0.3.0-RC4"]
                  [postgresql "9.0-801.jdbc4"]
                  [slingshot "0.10.3"]]
